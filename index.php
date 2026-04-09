a8c5ee7fc78fd48ea51d30d0bd811a301484ebc1
ACLC NORTHBAY <aclcnorthbayemailer@gmail.com>
2026-04-09 19:06:59 +0800

    add Dockerfile for mysqli

--- index.php
+++ index.php
@@ -3,18 +3,8 @@ ob_start();
 ini_set('display_errors', 0);
 error_reporting(0);
 
-$origin  = $_SERVER['HTTP_ORIGIN'] ?? '';
-$allowed = [
-    'https://aclc-attendance-monitoring-web.vercel.app',
-    'http://localhost',
-    'http://localhost:3000',
-    'http://127.0.0.1',
-];
-$allowOrigin = in_array($origin, $allowed)
-    ? $origin
-    : 'https://aclc-attendance-monitoring-web.vercel.app';
-
-header("Access-Control-Allow-Origin: $allowOrigin");
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+header("Access-Control-Allow-Origin: $origin");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Auth-Token, X-Requested-With");
@@ -31,14 +21,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = ltrim($uri, '/');
 
-// Root
 if ($uri === '' || $uri === 'index.php') {
     ob_end_clean();
     echo json_encode(['status' => 'ACLC Monitor API is running!', 'version' => '2.0']);
     exit;
 }
 
-// Any .php file at root level (setup.php, dbcheck.php, etc.)
 if (preg_match('/^[\w\-]+\.php$/', $uri)) {
     $file = __DIR__ . '/' . $uri;
     if (file_exists($file)) {
@@ -48,7 +36,6 @@ if (preg_match('/^[\w\-]+\.php$/', $uri)) {
     }
 }
 
-// api/* files
 if (strpos($uri, 'api/') === 0) {
     $file = __DIR__ . '/' . $uri;
     if (file_exists($file)) {
